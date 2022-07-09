66bc570a58aa80e59b46d6130cf4c8912b9eacb4
44ashish <ashishyadav4488@gmail.com>
2022-07-09 23:16:09 +0530

    Lab 5 Solution

---
+++ src/main/java/com/greatlearning/javafsd/studentmanagement/controller/StudentsController.java
@@ -0,0 +1,27 @@
+package com.greatlearning.javafsd.studentmanagement.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.greatlearning.javafsd.studentmanagement.entity.Student;
+import com.greatlearning.javafsd.studentmanagement.service.StudentService;
+
+@Controller
+@RequestMapping("/students")
+public class StudentsController {
+	
+	@Autowired
+	private StudentService studentService;
+	
+	@RequestMapping("/list")
+	public String handleListStudents(Model themodel) {
+		List<Student> students = studentService.listAll();
+		themodel.addAttribute("students", students);
+		return "students-lister";
+	}
+
+}
